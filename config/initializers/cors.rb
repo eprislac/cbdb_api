b94631148e1f0df2747eb5a12d439a545cae0ce4
Eddie Prislac <eddie@Eddies-MacBook-Pro.local>
2025-09-23 23:58:23 -0500

    turns off cors for now

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-rails.application.config.middleware.insert_before 0, rack.cors do
-  allow do
-    origins "cbdb.mine"
-
-    resource "*",
-      headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
-  end
-end
+# Rails.application.config.middleware.insert_before 0, rack.cors do
+#   allow do
+#     origins "cbdb.mine"
+#
+#     resource "*",
+#       headers: :any,
+#       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+#   end
+# end
