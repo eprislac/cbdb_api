8b9928382593908fdc6304dadc4dcba5beae1c11
Eddie Prislac <11530791+eprislac@users.noreply.github.com>
2025-10-03 01:47:27 -0500

    task(api):#2 - More changes for rspec tests

--- spec/routing/api/v1/by_lines_routing_spec.rb
+++ spec/routing/api/v1/by_lines_routing_spec.rb
@@ -7,24 +7,29 @@ RSpec.describe Api::V1::ByLinesController, type: :routing do
     end
 
     it "routes to #show" do
-      expect(api_v1_by_line(id: '1')).to route_to(controller: "api/v1/by_lines", action: "show", id: "1")
+      expect(api_v1_by_line_path(id: '1'))
+        .to route_to(controller: "api/v1/by_lines", action: "show", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(api_v1_create_by_line).to route_to(controller: "api/v1/by_lines", action: "create")
+      expect(api_v1_create_by_line_path)
+        .to route_to(controller: "api/v1/by_lines", action: "create")
     end
 
     it "routes to #update via PUT" do
-      expect(api_v1_update_by_line(id: '1')).to route_to(controller: "api/v1/by_lines", action: "update", id: "1")
+      expect(api_v1_update_by_line_path(id: '1'))
+        .to route_to(controller: "api/v1/by_lines", action: "update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(api_v1_update_by_line(id: '1')).to route_to(controller: "api/v1/by_lines", action: "update", id: "1")
+      expect(api_v1_update_by_line_path(id: '1'))
+        .to route_to(controller: "api/v1/by_lines", action: "update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(api_v1_delete_by_line(id: '1')).to route_to(controller: "api/v1/by_lines", action: "destroy", id: "1")
+      expect(api_v1_delete_by_line_path(id: '1'))
+        .to route_to(controller: "api/v1/by_lines", action: "destroy", id: "1")
     end
   end
 end
