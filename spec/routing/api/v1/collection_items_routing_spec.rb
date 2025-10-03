17eb70a7e074545f7a3fdcd0e81cab4befa4d80b
Eddie Prislac <11530791+eprislac@users.noreply.github.com>
2025-10-03 03:07:33 -0500

    task(api):#2 - More changes for rspec tests

--- spec/routing/api/v1/collection_items_routing_spec.rb
+++ spec/routing/api/v1/collection_items_routing_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "API V1 Collection Items Routes", type: :routing do
 
   describe "GET routes" do
     it "routes to index action" do
-      expect(get: "/api/v1/#{email}/collection/#{collection_id}/items").to route_to(
+      expect(get: "/api/v1/#{email}/collection/#{collection_id}/items", format: 'json').to route_to(
         controller: "api/v1/collection_items",
         action: "index",
         email: email,
@@ -20,7 +20,7 @@ RSpec.describe "API V1 Collection Items Routes", type: :routing do
     end
 
     it "routes to show action" do
-      expect(get: "/api/v1/#{email}/collection/#{collection_id}/items/#{item_id}").to route_to(
+      expect(get: "/api/v1/#{email}/collection/#{collection_id}/items/#{item_id}", format: 'json').to route_to(
         controller: "api/v1/collection_items",
         action: "show",
         email: email,
@@ -33,7 +33,7 @@ RSpec.describe "API V1 Collection Items Routes", type: :routing do
 
   describe "POST route" do
     it "routes to create action" do
-      expect(post: "/api/v1/#{email}/collection/#{collection_id}/items").to route_to(
+      expect(post: "/api/v1/#{email}/collection/#{collection_id}/items", format: 'json').to route_to(
         controller: "api/v1/collection_items",
         action: "create",
         email: email,
@@ -45,7 +45,7 @@ RSpec.describe "API V1 Collection Items Routes", type: :routing do
 
   describe "PUT route" do
     it "routes to update action" do
-      expect(put: "/api/v1/#{email}/collection/#{collection_id}/items/#{item_id}").to route_to(
+      expect(put: "/api/v1/#{email}/collection/#{collection_id}/items/#{item_id}", format: 'json').to route_to(
         controller: "api/v1/collection_items",
         action: "update",
         email: email,
@@ -58,7 +58,7 @@ RSpec.describe "API V1 Collection Items Routes", type: :routing do
 
   describe "DELETE route" do
     it "routes to destroy action" do
-      expect(delete: "/api/v1/#{email}/collection/#{collection_id}/items/#{item_id}").to route_to(
+      expect(delete: "/api/v1/#{email}/collection/#{collection_id}/items/#{item_id}", format: 'json').to route_to(
         controller: "api/v1/collection_items",
         action: "destroy",
         email: email,
