f3c3e6faa2f6814fc639fc745299407377233952
Eddie Prislac <11530791+eprislac@users.noreply.github.com>
2025-10-03 03:37:54 -0500

    task(api):#2 - Changes routing

--- spec/routing/api/v1/by_lines_routing_spec.rb
+++ spec/routing/api/v1/by_lines_routing_spec.rb
@@ -3,32 +3,33 @@ require "rails_helper"
 RSpec.describe Api::V1::ByLinesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get "/api/v1/by_lines").to route_to(controller: "api/v1/by_lines", action: "index", format: 'json')
+      expect(get: "/api/v1/by_lines", format: 'json')
+        .to route_to(controller: "api/v1/by_lines", action: "index", format: 'json')
     end
 
     it "routes to #show" do
-      expect(get "/api/v1/by_lines/1")
+      expect(get: "/api/v1/by_lines/1", format: 'json')
         .to route_to(controller: "api/v1/by_lines", action: "show", id: "1", format: 'json')
     end
 
 
     it "routes to #create" do
-      expect(post "/api/v1/by_lines/", params: { name: 'Writer' })
+      expect(post: "/api/v1/by_lines/", params: { name: 'Writer' }, format: 'json')
         .to route_to(controller: "api/v1/by_lines", action: "create", name: 'Writer', format: 'json')
     end
 
     it "routes to #update via PUT" do
-      expect(patch "/api/v1/by_lines/1")
+      expect(put: "/api/v1/by_lines/1", format: 'json')
         .to route_to(controller: "api/v1/by_lines", action: "update", id: "1", by_line: { name: 'Writer' }, format: 'json')
     end
 
     it "routes to #update via PATCH" do
-      expect(patch "/api/v1/by_lines/1")
+      expect(patch: "/api/v1/by_lines/1", format: 'json')
         .to route_to(controller: "api/v1/by_lines", action: "update", id: "1", by_line: { name: 'Writer' }, format: 'json')
     end
 
     it "routes to #destroy" do
-      expect(delete "/api/v1/by_lines/1")
+      expect(delete: "/api/v1/by_lines/1", format: 'json')
         .to route_to(controller: "api/v1/by_lines", action: "destroy", id: "1", format: 'json')
     end
   end
