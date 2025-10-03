60628145186ad69c13d325740013017c06e3fafc
Eddie Prislac <11530791+eprislac@users.noreply.github.com>
2025-10-03 00:15:25 -0500

    task(api):#2 - More changes for rspec tests

--- spec/routing/api/v1/collection_items_routing_spec.rb
+++ spec/routing/api/v1/collection_items_routing_spec.rb
@@ -8,28 +8,28 @@ RSpec.describe Api::V1::CollectionItemsController, type: :routing do
     end
 
     it "routes to #show" do
-      expect(get: api_v1_collection_items_path(collection_id: '1', id: '1'))
+      expect(get: api_v1_collection_item_path(collection_id: '1', id: '1'))
         .to route_to("api/v1/collections_items#show", id: '1', collection_id: '1')
     end
 
 
     it "routes to #create" do
-      expect(post: api_v1_create_collection_items_path(collection_id: '1'))
+      expect(post: api_v1_create_collection_item_path(collection_id: '1'))
         .to route_to("api/v1/collections_items#create", collection_id: '1')
     end
 
     it "routes to #update via PUT" do
-      expect(put: api_v1_update_collection_items_path(id: '1', collection_id: '1'))
+      expect(put: api_v1_update_collection_item_path(id: '1', collection_id: '1'))
         .to route_to("api/v1/collections_items#update", id: '1', collection_id: '1')
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: api_v1_update_collection_items_path(id: '1', collection_id: '1'))
+      expect(patch: api_v1_update_collection_item_path(id: '1', collection_id: '1'))
         .to route_to("api/v1/collections_items#update", id: '1', collection_id: '1')
     end
 
     it "routes to #destroy" do
-      expect(delete: api_v1_delete_collection_items_path(id: '1', collection_id: '1'))
+      expect(delete: api_v1_delete_collection_item_path(id: '1', collection_id: '1'))
         .to route_to("api/v1/collections_items#destroy", id: '1', collection_id: '1')
     end
   end
