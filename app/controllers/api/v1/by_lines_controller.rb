a4a2cce89d99b9cf87a293dd504376b5cb0d0e8c
Eddie Prislac <11530791+eprislac@users.noreply.github.com>
2025-10-04 11:39:19 -0500

    task(api):#2 - Fixes more tests

--- app/controllers/api/v1/by_lines_controller.rb
+++ app/controllers/api/v1/by_lines_controller.rb
@@ -6,36 +6,46 @@ module Api
         @by_lines = ByLine.all
 
         render json: @by_lines
+      rescue StandardError => e
+        Rails.logger.error("ERROR: #{e.message}")
+        render json: { error: e.message }, status: :unprocessable_entity
       end
 
       # GET /by_lines/1
       def show
         render json: by_line
+      rescue StandardError => e
+        Rails.logger.error("ERROR: #{e.message}")
+        render json: { error: e.message }, status: :unprocessable_entity
       end
 
       # POST /by_lines
       def create
         @by_line = ByLine.new(by_line_params)
 
-        if @by_line.save
-          render json: @by_line, status: :created, location: @by_line
-        else
-          render json: @by_line.errors, status: :unprocessable_entity
-        end
+        @by_line.save!
+        render json: @by_line, status: :created, location: @by_line
+      rescue StandardError => _e
+        Rails.logger.error("ERROR: #{@by_line.errors.full_messages}")
+        render json: @by_line.errors, status: :unprocessable_entity
       end
 
       # PATCH/PUT /by_lines/1
       def update
-        if by_line.update(by_line_params)
-          render json: by_line
-        else
-          render json: by_line.errors, status: :unprocessable_entity
-        end
+        by_line.update!(by_line_params)
+        render json: by_line
+      rescue StandardError => e
+        Rails.logger.error("ERROR: #{by_line.errors.full_messages}")
+        render json: by_line.errors, status: :unprocessable_entity
       end
 
       # DELETE /by_lines/1
       def destroy
         by_line.destroy!
+        render json: { message: "By line deleted successfully" }, status: :ok
+      rescue StandardError => e
+        Rails.logger.error("ERROR: #{e.message}")
+        render json: { error: e.message }, status: :unprocessable_entity
       end
 
       private
@@ -47,7 +57,7 @@ module Api
 
       # Only allow a list of trusted parameters through.
       def by_line_params
-        params.expect(:by_line).permit(:name)
+        params.require(:by_line).permit(:name)
       end
     end
   end
--- spec/requests/api/v1/by_lines_spec.rb
+++ spec/requests/api/v1/by_lines_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "api/v1/by_lines", type: :request do
-  let(:valid_attributes) { { name: 'Writer' } }
+  let!(:valid_attributes) { { name: 'Writer' } }
 
-  let(:invalid_attributes) { { name: '' } }
+  let!(:invalid_attributes) { { name: '' } }
 
 
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
   # ByLinesController, or in your router and rack
   # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
+  let!(:valid_headers) {
     { "ACCEPT" => "application/json" }
   }
 
-  let(:by_line) { create(:by_line, valid_attrubutes) }
+  let(:by_line) { create(:by_line, valid_attributes) }
 
   describe "GET /index" do
     it "renders a successful response" do
@@ -56,7 +56,7 @@ RSpec.describe "api/v1/by_lines", type: :request do
       end
 
       it "renders a JSON response with errors for the new by_line" do
-        post by_lines_url,
+        post api_v1_by_lines_url,
              params: { by_line: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_content)
         expect(response.content_type).to match(a_string_including("application/json"))
@@ -70,16 +70,16 @@ RSpec.describe "api/v1/by_lines", type: :request do
         { name: "Artist" }
       }
 
-      let(:by_line) { create(:by_line, name: 'Editor') }
+      let!(:by_line) { create(:by_line, name: 'Editor') }
       it "updates the requested by_line" do
-        patch by_line_url(by_line),
+        patch api_v1_by_line_url(by_line),
               params: { by_line: new_attributes }, headers: valid_headers, as: :json
         by_line.reload
         expect(by_line.name).to eq("Artist")
       end
 
       it "renders a JSON response with the by_line" do
-        patch by_line_url(by_line),
+        patch api_v1_by_line_url(by_line),
               params: { by_line: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to match(a_string_including("application/json"))
@@ -87,7 +87,7 @@ RSpec.describe "api/v1/by_lines", type: :request do
     end
 
     context "with invalid parameters" do
-      let(:by_line) { create(:by_line, name: 'Editor') }
+      let!(:by_line) { create(:by_line, name: 'Editor') }
       it "renders a JSON response with errors for the by_line" do
         patch api_v1_by_line_url(by_line),
               params: { by_line: invalid_attributes }, headers: valid_headers, as: :json
@@ -98,11 +98,11 @@ RSpec.describe "api/v1/by_lines", type: :request do
   end
 
   describe "DELETE /destroy" do
-    let(:by_line) { create(:by_line, name: 'Editor') }
+    let!(:by_line) { create(:by_line, name: 'Editor') }
     it "destroys the requested by_line" do
       expect {
-        delete by_line_url(by_line), headers: valid_headers, as: :json
-      }.to change(ByLine, :count).by(-1)
+        delete api_v1_by_line_url(by_line), headers: valid_headers, as: :json
+      }.to change(ByLine.all, :count).by(-1)
     end
   end
 end
